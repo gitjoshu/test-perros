dd63801856645fd49fc2cf6902265ce8f5050968
gitjoshu <joshua.munoz.barbero@gmail.com>
2021-12-06 12:28:16 +0100

    fix initial select value

--- src/components/ImagesGallery.jsx
+++ src/components/ImagesGallery.jsx
@@ -15,24 +15,26 @@ export const ImagesGallery = forwardRef((props, ref) => {
               : ""
           }`}
         >
-          {props.images.length === 0 && ref.current !== undefined && (
-            <>
-              <div className="col-12 d-flex justify-content-center">
-                NO HAY IMÁGENES DEL TIPO DE RAZA {ref.current}, PERO PODEMOS
-                MOSTRAR TODAS LAS IMÁGENES QUE HAY DE SU RAZA
-              </div>
-              <div className="col-12 d-flex justify-content-center mt-3">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => props.changeImages(ref.current, "breed")}
-                >
-                  {ref.current !== undefined && (
-                    <>{ref.current.split("-")[0]}</>
-                  )}
-                </button>
-              </div>
-            </>
-          )}
+          {props.images.length === 0 &&
+            ref.current !== undefined &&
+            ref.current !== "" && (
+              <>
+                <div className="col-12 d-flex justify-content-center">
+                  NO HAY IMÁGENES DEL TIPO DE RAZA {ref.current}, PERO PODEMOS
+                  MOSTRAR TODAS LAS IMÁGENES QUE HAY DE SU RAZA
+                </div>
+                <div className="col-12 d-flex justify-content-center mt-3">
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => props.changeImages(ref.current, "breed")}
+                  >
+                    {ref.current !== undefined && (
+                      <>{ref.current.split("-")[0]}</>
+                    )}
+                  </button>
+                </div>
+              </>
+            )}
           <ImagesList images={props.images} />
         </div>
       )}
